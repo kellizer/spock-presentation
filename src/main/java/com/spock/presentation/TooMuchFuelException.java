a54b8bb32194cc8e01ba50fc977d894bdc8f0eb1
Ian <ianwesleykelly@gmail.com>
2013-06-26 14:49:09 +0100

    New Changes..

---
+++ src/main/java/com/spock/presentation/TooMuchFuelException.java
@@ -0,0 +1,11 @@
+package com.spock.presentation;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: iank
+ * Date: 26/06/13
+ * Time: 14:36
+ * To change this template use File | Settings | File Templates.
+ */
+public class TooMuchFuelException extends RuntimeException {
+}
---
+++ src/test/groovy/com/spock/presentation/FuelRatioTests.groovy
@@ -0,0 +1,13 @@
+package com.spock.presentation
+
+import spock.lang.Specification
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: iank
+ * Date: 26/06/13
+ * Time: 14:48
+ * To change this template use File | Settings | File Templates.
+ */
+class FuelRatioTests extends Specification {
+}
