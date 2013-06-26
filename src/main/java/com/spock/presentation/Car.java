456bc612a186104151b45a777dcf675481332cfa
Ian <kellizer@gmail.com>
2013-06-26 14:53:49 +0100

    New Changes..

--- src/main/java/com/spock/presentation/Car.java
+++ src/main/java/com/spock/presentation/Car.java
@@ -1,5 +1,7 @@
 package com.spock.presentation;
 
+import java.math.BigDecimal;
+
 /**
  * Created with IntelliJ IDEA.
  * Represents a Car domain.
@@ -7,6 +9,10 @@ package com.spock.presentation;
 public class Car {
 
     private static final int LEGAL_AGE = 16;
+    private BigDecimal milesPerGallonRatio = new BigDecimal(0);
+    private BigDecimal fuelTankSize = new BigDecimal(0);
+    private BigDecimal currentFuel = new BigDecimal(0);
+
 
     /**
      * Sets the primary Driver for the car.
@@ -18,5 +24,22 @@ public class Car {
             throw new DriverTooYoungException(LEGAL_AGE, driver.getAge());
         if (!driver.isDrivingLicenceValid())
             throw new DriverLicenceInvalidException();
+    }
+
+    public void addFuel(final BigDecimal additionalPetrol) {
+        BigDecimal newFuelCount = currentFuel.add(additionalPetrol);
+        if (newFuelCount.compareTo(fuelTankSize) > 0)
+            throw new TooMuchFuelException();
+        currentFuel = newFuelCount;
+    }
+
+
+    /**
+     * Sets the ratio for this car.
+     *
+     * @param milesPerGallonRatio - the ratio for mile to a gallon of fuel.
+     */
+    public void setMilesPerGallonRatio(BigDecimal milesPerGallonRatio) {
+        this.milesPerGallonRatio = milesPerGallonRatio;
     }
 }
